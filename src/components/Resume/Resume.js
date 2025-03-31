ab9cf7a7aed82d97c97d04cd6926582e6658fc03
BabyLucyJoan <nwokoyelucy@gmail.com>
2025-03-31 23:53:54 +0100

    Today

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./Resume.module.css";
+import BackButton from "./BackButton/BackButton";
 
 function Resume() {
   const resumeUrl = "/resume.pdf";
 
   return (
     <div className={styles.resumeContainer}>
+      <BackButton />
       <iframe src={resumeUrl} title="Resume" className={styles.resumeViewer} />
     </div>
   );
